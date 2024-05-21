87949f91845598af6bbff00aa56c6e21141e28d8
siddhant124 <siddhantgoyal3364@gmail.com>
2024-05-22 00:02:32 +0530

    practice code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1>this is glbal layout header</h1>
+        {children}
+        <h1>this is glbal layout footer</h1>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
-export default function Home() {
+
+async function setTimeoutFun() {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+}
+
+export default async function Home() {
+  await setTimeoutFun();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
